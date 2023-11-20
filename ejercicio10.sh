e613bbc05265d635b7baab258776763b2828afe2
intmarisma <150593148+intmarisma@users.noreply.github.com>
2023-11-20 09:52:26 +0100

    Add files via upload

--- ejercicio10.sh
+++ ejercicio10.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Verifica si se proporiconaron parámetros
+if [ $# -eq 0 ]; then
+   echo "No se proporcioanron parámetros."
+   exit 1
+fi
+
 # Obtener el primer parámetro
 directorio=$1
 
